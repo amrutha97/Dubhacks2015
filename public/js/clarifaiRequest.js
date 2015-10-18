56756641015b9dddf139bb452d542e37f1594bd3
Ac2zoom <ac2zoom@yahoo.com>
2015-10-18 15:37:22 -0700

    Moved clarifaiRequest.js

--- public/js/clarifaiRequest.js
+++ public/js/clarifaiRequest.js
@@ -1,22 +1,11 @@
-/*$(document).ready(
-    function(){
-        var clarifai = new Clarifai(
-            {
-                'clientId': '7b5Y1TIXChj8yhXGdcRldG5t7wzJWTtH8',
-                'clientSecret': 'a5rz9I8_aJnJqh6EUn1nNponqSVjZiuOPSrmsCE3'
-            }
-        );
-    }
-);*/
-
-
 
 function getAccessToken() {
+    var returnVal;
     var formData = {
         grant_type: "client_credentials",
         client_id: "XqUKUx-7b5Y1TIXChj8yhXGdcRldG5t7wzJWTtH8",
         client_secret: "a5rz9I8_aJnJqh6EUn1nNponqSVjZiuOPSrmsCE3",
-    };
+        };
     jQuery.ajax({
         url: "https://api.clarifai.com/v1/token/",
         type: "POST",
@@ -24,28 +13,40 @@ function getAccessToken() {
         async: false,
         success: function(data, textStatus, jqXHR) {
             // do stuff with data
-            imageRequest("http://i.imgur.com/tUfxxLd.jpg", data["access_token"]);
+            returnVal =  data['access_token'];
+
         },
         error: function(jqXHR, textStatus, errorThrown) {
             // do stuff with error
+            returnVal = errorThrown;
         }
     });
+    return returnVal;
+
 }
 
-function imageRequest(url, token) {
-    var formData = {
-        Authorization: "Bearer" + token,
-    }
+function imageRequest(photoUrl, token) {
+    var returnVal;
     jQuery.ajax({
         url: "https://api.clarifai.com/v1/tag/",
         type: "post",
+        beforeSend: function(xhr) {
+            // console.log('Bearer '.concat(token));
+            xhr.setRequestHeader('Authorization', 'Bearer '.concat(token));
+        },
         data: {
-
+            url: photoUrl
         },
         dataType: 'json',
-        async: true,
+        async: false,
         success: function(data) {
-
+            returnVal = data;
+        },
+        error: function(jqXHR, textStatus, errorThrow) {
+            returnVal = errorThrow;
         }
-    })
+    });
+    return returnVal;
 }
+
+
