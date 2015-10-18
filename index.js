7df951fd7c42e5936d7d75597c2d4b58ec2a13da
Ac2zoom <ac2zoom@yahoo.com>
2015-10-18 01:04:38 -0700

    Latest Instagram pull request

--- index.js
+++ index.js
@@ -4,17 +4,24 @@ function loadImages(){
     // $.get( "https://instagram.com/oauth/authorize/?client_id=" + instagram_client_id + "&redirect_uri=http://www.compatigram.co&response_type=token", function( data ) {
     //   console.log(data);
     // });
-    jQuery.ajax({
-        url: "https://instagram.com/oauth/authorize/?client_id=" + instagram_client_id + "&redirect_uri=http://www.compatigram.co&response_type=token",
-        type: "GET",
+    // jQuery.ajax({
+    //     url: "https://instagram.com/oauth/authorize/?client_id=" + instagram_client_id + "&redirect_uri=http://www.compatigram.co&response_type=token",
+    //     type: "GET",
+    //     headers: {Access-Control-Allow-Origin:'http://www.compatigram.co/'},
+    //     async: false,
+    //     success: function(data, textStatus, jqXHR) {
+    //         // do stuff with data
+    //         console.log(data.access_token);
+    //     },
+    //     error: function(jqXHR, textStatus, errorThrown) {
+    //         // do stuff with error
+    //     }
+    // });
+    $.ajax({
+        url: "https://instagram.com/oauth/authorize/?client_id=" + instagram_client_id + "&redirect_uri=http://localhost&response_type=token",
         headers: {'Access-Control-Allow-Origin':'http://www.compatigram.co/'},
-        async: false,
         success: function(data, textStatus, jqXHR) {
-            // do stuff with data
-            console.log(data.access_token);
+            console.log(data);
         },
-        error: function(jqXHR, textStatus, errorThrown) {
-            // do stuff with error
-        }
     });
 }
