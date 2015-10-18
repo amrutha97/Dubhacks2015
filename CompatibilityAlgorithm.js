29bb7f4f17e9aa06172e91dc67e35314ff326dd2
Amrutha Gujjar <amruthasg@yahoo.com>
2015-10-18 05:57:18 -0700

    compare method fixes

--- CompatibilityAlgorithm.js
+++ CompatibilityAlgorithm.js
@@ -1,9 +1,19 @@
-function computeCompatibility(firstAvgs, secondAvgs) {    
+function computeSimilarity(firstAvgs, secondAvgs) {    
     var compatibility = 0;
     for (var avg in firstAvgs) {      
-        if(firstAvgs[avg] - secondAvgs[avg] < 5) {
+        if(//comparing  
+           //
+        ) {
             compatibility++;
         }
     }
     return compatibility;            
 }
+
+function compare() {
+
+}
+
+
+
+
