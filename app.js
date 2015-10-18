dd6e943ea59d317cb83260633375763c0d71add6
Ac2zoom <ac2zoom@yahoo.com>
2015-10-18 02:22:23 -0700

    Changed stuff up for Heroku compatibility

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var express = require('express'),
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
     res.render('index', {});
 });
+
+app.listen(8080);
