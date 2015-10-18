14fdfcfdf52c12e926120ec2f26263d9f5fc6dfe
pspieker <PatrickSpieker@users.noreply.github.com>
2015-10-17 23:53:22 -0700

    Added clarifai stuff

---
+++ claifaiRequest.js
@@ -0,0 +1,41 @@
+
+function getAccessToken() {
+    var formData = {
+        grant_type: "client_credentials",
+        client_id: "XqUKUx-7b5Y1TIXChj8yhXGdcRldG5t7wzJWTtH8",
+        client_secret: "a5rz9I8_aJnJqh6EUn1nNponqSVjZiuOPSrmsCE3",
+        };
+    jQuery.ajax({
+        url: "https://api.clarifai.com/v1/token/",
+        type: "POST",
+        data: formData,
+        async: false,
+        success: function(data, textStatus, jqXHR) {
+            // do stuff with data
+            imageRequest("http://i.imgur.com/tUfxxLd.jpg", data["access_token"]);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            // do stuff with error
+        }
+    });
+}
+
+function imageRequest(url, token) {
+    var formData = {
+        Authorization: "Bearer" + token,
+    }
+    jQuery.ajax({
+        url: "https://api.clarifai.com/v1/tag/",
+        type: "post",
+        data: {
+
+        },
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+
+        }
+    })
+}
+
+getAccessToken();
