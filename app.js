7da8e973aa96cf6b0333ee8a0f9ce963705f13b4
Ac2zoom <ac2zoom@yahoo.com>
2015-10-18 03:34:14 -0700

    Finally got server working properly

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express = require('express'),
     app = express(),
-    cons = require('consolidate'),
-    swig = require('swig');
+    engines = require('consolidate');
 
-app.engine('html', cons.swig);
-app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
     res.render('index', {});
