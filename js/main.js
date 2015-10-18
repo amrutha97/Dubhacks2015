8ca555c8c4faf49ee9c20ee9311db898a26841c3
anitaleung <leungak@cs.uw.edu>
2015-10-18 05:20:06 -0700

    add small emoji lol

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+    // HEY! Change similarity based on the percentage returned
+    // after Danny and Amrutha figure out how to return the
+    // percentage for similarity
+    var similarity = 65; // Adjust this
+
+    // Adjust similarity bar
+    var simPercent = similarity + '%';
+    $('#meter').width(simPercent);
+    $('#percent').html(simPercent);
+
+
     // Replace profile picture, names, and handler with input JSON files
     $.getJSON('js/result.json', function(data) {
         $('#p1-img').attr('src', data.data.profile_picture);
