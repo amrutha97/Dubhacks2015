25ed0deb3a4235b108d5bc58a77c4aec9ab8aff3
Ac2zoom <ac2zoom@yahoo.com>
2015-10-18 15:25:02 -0700

    Heroku testing

--- public/js/instagramPull.js
+++ public/js/instagramPull.js
@@ -22,14 +22,12 @@ function retrieveUserInfo(){
 
 function retrieveOtherUserInfo(){
     var currentHandle = $('#otherUserHandle').val();
-    console.log(currentHandle);
     $.ajax({
         type: "GET",
         dataType: "jsonp",
         cache: false,
         url: "https://api.instagram.com/v1/users/search?q=" + currentHandle + "&count=1&access_token="+str,
         success: function(data) {
-            console.log(data);
             $('#p2-img').attr('src', data.data[0].profile_picture);
             $('#p2-name').html(data.data[0].full_name);
             $('#p2-handler').html('@' + data.data[0].username);
@@ -46,6 +44,7 @@ function retrieveOtherUserImages(){
         cache: false,
         url: "https://api.instagram.com/v1/users/" + otherID + "/media/recent/?client_id=b3ff009db8c3416e87f3b1625a475294&access_token="+str,
         success: function(data) {
+            console.log(data);
             data['data'][0]['images']['standard_resolution']['url']
             for(var i = 0; i < data['data'].length; i++){
                 otherImageList.push(data['data'][i]['images']['standard_resolution']['url']);
