c644532a4e467349de5d190890a74da8d2003962
Ac2zoom <ac2zoom@yahoo.com>
2015-10-18 13:27:52 -0700

    Initial commit of retrieveUserInfo()

--- public/js/instagramPull.js
+++ public/js/instagramPull.js
@@ -1,3 +1,20 @@
+var selfID;
+var selfUsername;
+var profilePicture;
+
 function retrieveUserInfo(){
-    
+    $.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url: "https://api.instagram.com/v1/users/self/?access_token="+str,
+        success: function(data) {
+            selfID = data['data']['id'];
+            selfUsername = data['data']['username'];
+            profilePicture = data['data']['profilePicture'];
+        }
+    });
+
+    $('.input-handler').val(selfUsername);
+    $('#p1-img').attr('src',profilePicture);
 }
