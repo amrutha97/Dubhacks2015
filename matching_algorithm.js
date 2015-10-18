089cb65d1671bee432b9d3eefd4424646283cefb
Daniel Allen <dtallen97@users.noreply.github.com>
2015-10-18 01:26:26 -0700

    Updated matching_algorithm.js Created a function to compute average frequency for each tag.

--- matching_algorithm.js
+++ matching_algorithm.js
@@ -8,8 +8,9 @@
  */
 var merge = function(imageTags, userTags) {
     for (var index in imageTags) {
+        // hacking tag b/c js is stupid and gets the index not the var
         var tag = userTags[index];
-        if(tagMap.hasOwnProperty(tag)) {
+        if(tagMap.hasOwnProperty(tag)) { // checks if the tag exists already
             userTags[tag]++;
         } else {
             userTags[tag] = 1;
@@ -18,10 +19,19 @@ var merge = function(imageTags, userTags) {
     return userTags;
 };
 
+/**
+ * Computes the average frequency of each tag associated with a user's images.
+ * @param userTags a map which contains all of a users tags and the
+ *        frequency of each tag
+ * @param totalPhotos the total number of images a user has on their profile
+ * @returns a map with the average frequency of each tag associated with a user
+ */
 var computeAverageFrequencies = function(userTags, totalPhotos) {
     for (var tag in userTags) {
-
+        userTags[tag] /= totalPhotos;
     }
+
+    return userTags;
 }
 
 
