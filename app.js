1d31087f38c10110caa4e718ce45a4d50395cbbc
Ac2zoom <ac2zoom@yahoo.com>
2015-10-18 05:55:38 -0700

    Added item with id access_token

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('views', __dirname);
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res){
-    res.render('index', {location.hash});
+    res.render('index', {});
 });
 
 app.listen(process.env.PORT || 8080);
